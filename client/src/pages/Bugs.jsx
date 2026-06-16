4dd8b5292723d85746f7648abf69d9c3c020b587
Gautam Kochar <gautam.kochar@girnarsoft.com>
2026-06-16 22:51:52 +0530

    Add React Vite frontend

---
+++ client/src/pages/Bugs.jsx
@@ -0,0 +1,5 @@
+function Bugs() {
+  return <h1>Bugs</h1>;
+}
+
+export default Bugs;
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>QA Analytics Dashboard</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
